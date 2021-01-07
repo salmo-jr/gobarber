5ed74cd5b9a0021ce8b5614704fb2f0c095b8769
Salmo Marques <salmo.sjunior@sp.senac.br>
2021-01-06 21:28:57 -0300

    initial commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+/**
+ * Rotas: receber requisições, encaminha-las, retornar respostas
+ */
+
+appointmentsRouter.get('/', (request, response) => {
+    const appointments = appointmentsRepository.all();
+    return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+    try {
+        const { provider, date } = request.body;
+
+        const parseDate = parseISO(date);
+
+        const createAppointment = new CreateAppointmentService(
+            appointmentsRepository,
+        );
+        const appointment = createAppointment.execute({
+            date: parseDate,
+            provider,
+        });
+        return response.json(appointment);
+    } catch (err) {
+        return response.status(400).json({ error: err.message });
+    }
+});
+
+export default appointmentsRouter;
