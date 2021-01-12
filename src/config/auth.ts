19272c624e071e2490d42873c2929d68d8158bb4
salmo jr <salmo.jr@gmail.com>
2021-01-12 20:33:34 -0300

    chore: implements an authentication system

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'e49ff84156613ca0f026e6009dd18b98',
+        expiresIn: '1d',
+    },
+};
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -3,8 +3,10 @@ import { parseISO } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const appointmentsRouter = Router();
+appointmentsRouter.use(ensureAuthenticated);
 
 /**
  * Rotas: receber requisições, encaminha-las, retornar respostas
