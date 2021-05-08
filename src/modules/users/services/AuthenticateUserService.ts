cd516912ffd17c0e29594952b66c5166df9c1a3f
salmo jr <salmo.jr@gmail.com>
2021-05-08 16:42:15 -0300

    refactor: refactors the users module

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,25 +1,26 @@
-import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 import AppError from '@shared/errors/AppError';
 
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUserRepository';
 
-interface RequestDTO {
+interface IRequestDTO {
     email: string;
     password: string;
 }
 
-interface Response {
+interface IResponse {
     user: User;
     token: string;
 }
 
 class AuthenticateUserService {
-    public async execute({ email, password }: RequestDTO): Promise<Response> {
-        const userRepository = getRepository(User);
-        const user = await userRepository.findOne({ where: { email } });
+    constructor(private usersRepository: IUsersRepository) {}
+
+    public async execute({ email, password }: IRequestDTO): Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
         if (!user)
             throw new AppError('Incorrect email/password combination', 401);
 
--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,32 +1,33 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUserRepository';
 
-interface RequestDTO {
+interface IRequestDTO {
     name: string;
     email: string;
     password: string;
 }
 
 class CreateUserService {
-    public async execute({ name, email, password }: RequestDTO): Promise<User> {
-        const userRepository = getRepository(User);
-        const checkUsersExist = await userRepository.findOne({
-            where: { email },
-        });
+    constructor(private usersRepository: IUsersRepository) {}
+
+    public async execute({
+        name,
+        email,
+        password,
+    }: IRequestDTO): Promise<User> {
+        const checkUsersExist = await this.usersRepository.findByEmail(email);
 
         if (checkUsersExist) throw new AppError('Email address already used.');
 
         const hashedPassword = await hash(password, 8);
 
-        const user = userRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password: hashedPassword,
         });
-
-        await userRepository.save(user);
 
         return user;
     }
